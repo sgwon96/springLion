9b8644f8b48b8ca7674e63f3078e25274dd30c02
RunaNam <nmj5012@naver.com>
2021-04-20 00:34:40 +0900

    [min]~ 스프링으로 전환하기

--- minjeong/week1/core/src/main/java/hello/core/AppConfig.java
+++ minjeong/week1/core/src/main/java/hello/core/AppConfig.java
@@ -1,18 +1,36 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
-    public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberServiceImpl(getMemberRepository());
     }
 
-    public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    @Bean
+    public MemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceImpl(getMemberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
