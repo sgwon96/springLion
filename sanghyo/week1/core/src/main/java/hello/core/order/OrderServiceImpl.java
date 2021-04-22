d08dff29b79a903fdd2b8dea4ab94cd836a73fbe
Sanghyo <sanghyoboy@naver.com>
2021-04-22 12:00:24 +0900

    조회된 bean이 여러개인 경우 처리

--- sanghyo/week1/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ sanghyo/week1/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,14 +1,17 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
