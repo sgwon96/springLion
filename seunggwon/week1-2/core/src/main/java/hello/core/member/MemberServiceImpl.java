51f3cc9c5e4bf1774d0aa6ce6b03e8586d5d39e6
sgwon96 <zxcvb5434@likelion.org>
2021-04-24 15:25:50 +0900

    [gwon]ConponentScan

--- seunggwon/week1-2/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ seunggwon/week1-2/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,5 +1,9 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
@@ -10,6 +14,7 @@ public class MemberServiceImpl implements MemberService{
         return memberRepository;
     }
 
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
